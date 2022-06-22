46e28ee624cf78414620a8585f38e8ac008a2666
Chris Artates <ch1artates@gmail.com>
2022-06-22 11:21:48 +0800

    wus good

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const gridContainer = document.querySelector(".grid-container");
 const resetBtn = document.querySelector(".reset-btn");
+const gridBtn = document.querySelector(".grid-btn");
+const changeColorBtn = document.querySelector(".changeColor-btn");
+const blackBtn = document.querySelector(".black-btn");
+
+let gridSize = 16;
+let isFunMode = false;
+
 const generateGrid = () => {
-  for (let i = 0; i <= 255; i++) {
+  for (let i = 0; i <= gridSize * gridSize; i++) {
     const gridItem = document.createElement("div");
     gridItem.setAttribute("class", "grid-item");
     gridItem.addEventListener("mouseover", changeColor);
@@ -10,7 +17,39 @@ const generateGrid = () => {
 };
 
 function changeColor(e) {
-  e.target.style.backgroundColor = "black";
+  if (!isFunMode) {
+    e.target.style.backgroundColor = "black";
+  } else {
+    e.target.style.backgroundColor = randomColor();
+  }
 }
 
+function randomColor() {
+  let randColor = "";
+  for (let i = 0; i < 6; i++) {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    randColor = `rgb(${r} ${g} ${b})`;
+  }
+
+  return randColor;
+}
+
+function resetColors() {
+  let x = document.querySelectorAll(".grid-item");
+  x.forEach((item) => {
+    item.style.backgroundColor = "white";
+  });
+}
+
+resetBtn.addEventListener("click", resetColors);
+
+changeColorBtn.addEventListener("click", () => {
+  isFunMode = !isFunMode;
+  if (isFunMode) changeColorBtn.textContent = "Turn off fun mode :C";
+  else changeColorBtn.textContent = "Fun mode!";
+});
+
 generateGrid();
+randomColor();
